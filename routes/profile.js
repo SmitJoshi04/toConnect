a78d47c792083e8b2512b3a3fffb4df82b1d2838
Darkm0nk <vishal.pansheriya17916@marwadieducation.edu.in>
2024-03-06 21:59:25 +0530

    create different routes for all 3 api

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const {getUserPost} = require('../controllers/profile');
+const {getUserPost, getUserFriend, getUserProfileData} = require('../controllers/profile');
 const { verifyToken } = require('../middlewares/user');
 
 const router = express.Router();
 
 router.post('/',verifyToken,getUserPost);
+router.post('/friends',verifyToken,getUserFriend);
+router.post('/profile',verifyToken,getUserProfileData);
+
 
 module.exports = router;
