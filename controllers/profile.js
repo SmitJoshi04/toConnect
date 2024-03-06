6246acfd0f23cb7f3d5a35ecb3a77dabf0bf3118
Darkm0nk <vishal.pansheriya17916@marwadieducation.edu.in>
2024-03-06 21:58:05 +0530

    add different apis for userProfileData, userPost, userFriendData

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,51 +1,42 @@
 const dbcon = require("../connection");
 
-// const getPosts = (req,res)=>{
-//     let user = req.user;
-//     console.log(user);
-//     const query = `select * from post where u_id= ${user.userId} order by post_id desc;`
-//     dbcon.getConnection((err,con)=>{
-//         if (err){
-//             con.release()
-//             return res.send(err);
-//         }
-//         con.query(query,(err,result)=>{
-//             if (err){
-//                 con.release()
-//                 return res.send(err);
-//             }
-//             res.json(result);
-//         })
-//     })
-// }
+const getUserProfileData = async function (req,res){
+    let user = req.user;
+    console.log(user);
+    const query = `select * from user where u_id= ${user.userId}`
+    await dbcon.getConnection((err, con) => {
+        if (err) {
+            con.release()
+            return res.send(err);
+        }
+        con.query(query, (err, result) => {
+            if (err) {
+                con.release()
+                return res.send(err);
+            }
+            res.send(result);
+            con.release();
+            console.log("resultof user data::", result);
+            // response = result;
+            // console.log("response of user data:::", response);
+        })
+
+})
+}
+
 const getUserPost = async function (req, res) {
 
     var getPostArray = [];
     let user = req.user;
-    let response=[];
-    console.log("req.user ::: ",user);
-    const query = `select * from user where u_id= ${user.userId}`
-    await dbcon.getConnection((err,con)=>{
-        if (err){
-            con.release()
-            return res.send(err);
-        }
-        con.query(query,(err,result)=>{
-            if (err){
-                con.release()
-                return res.send(err);
-            }
-            console.log("resultof user data::",result);
-            response= result;
-            console.log("response of user data:::",response);
-        })
-    
-    
-    // const userId = req.user.userId
+    // let response = [];
+    console.log("req.user ::: ", user);
+   
 
-    // console.log("userid :: ", userId);
+        // const userId = req.user.userId
 
-    
+        // console.log("userid :: ", userId);
+
+        await dbcon.getConnection((err, con) => {
         con.query(`SELECT * FROM post WHERE u_id =?`, [user.userId], async (err, result) => {
             if (err)
                 console.error(err);
@@ -53,11 +44,11 @@ const getUserPost = async function (req, res) {
             console.log((result));
 
 
-            
+
             async function getCommentsToPost(post_id) {
                 return new Promise((resolve, reject) => {
                     console.log("::::::::::::::::::::", post_id);
-                     con.query(`SELECT * FROM comments WHERE post_id=?`, [post_id], (err2, resultComment) => {
+                    con.query(`SELECT * FROM comments WHERE post_id=?`, [post_id], (err2, resultComment) => {
                         console.log("::::::::::::::::::::");
                         if (err2)
                             return reject(err2);
@@ -73,7 +64,7 @@ const getUserPost = async function (req, res) {
                 return new Promise((resolve, reject) => {
                     console.log("::::::::::::::::::::", post_id);
 
-                     con.query(`SELECT * FROM likes WHERE post_id=?`, [post_id], (err3, resultlike) => {
+                    con.query(`SELECT * FROM likes WHERE post_id=?`, [post_id], (err3, resultlike) => {
                         console.log("::::::::::::::::::::");
                         if (err3)
                             return reject(err2);
@@ -91,20 +82,20 @@ const getUserPost = async function (req, res) {
 
                 // result[i].post = result[i];
 
-                console.log("result[i].post", result[i].post );
-                console.log("First Element of Post Array Result ", result[i].post); 
+                console.log("result[i].post", result[i].post);
+                console.log("First Element of Post Array Result ", result[i].post);
 
 
-                
+
                 const postId = result[i].post_id
                 console.log("post_id", postId);
                 commentResult = await getCommentsToPost(postId);
-                
+
 
                 result[i].comments = commentResult;
                 console.log('commentResult ::::: ', commentResult);
 
-                
+
                 // console.log("[result[i][0].post_id]",result[i][0].post_id);
 
                 console.log("Comment Element of Post Array Result ", result[i].comments);
@@ -112,25 +103,49 @@ const getUserPost = async function (req, res) {
                 likeResult = await getLikesToPost(postId);
 
                 result[i].likes = likeResult;
-                console.log("likeResult",likeResult);
+                console.log("likeResult", likeResult);
 
                 console.log("like Element of Post Array Result ", result[i].likes);
 
                 console.log("Element of Post Array Result ", result[i]);
-                
+
                 getPostArray.push(result[i]);
                 console.log("getPostArray", getPostArray);
 
             }
             // res.send(getPostArray);
             console.log("getPostArray", getPostArray);
-            response.push(getPostArray);
-            console.log("whole response:::",response);
+            res.send(getPostArray);
+            // response.push(getPostArray);
+            // console.log("whole response:::", response);
             con.release();
-            res.json(response);
+            // res.json(response);
         })
     })
 }
 
 
-module.exports = {getUserPost};
+
+const getUserFriend = async function (req, res) {
+    let user = req.user;
+    await dbcon.getConnection((err, con) => {
+        if (err) {
+            con.release()
+            return res.send(err);
+        }
+        con.query(`SELECT user1_id FROM friend WHERE user2_id =? AND status =?`, [user.userId, 'A'], async (err, result) => {
+            if (err) {
+                console.error(err);
+                con.release();
+
+            }
+            console.log("result of no. of friends::", result);
+            res.send(result);
+            // response.push(result);
+            // console.log("response of friend and user data:::", response);
+
+        })
+    })
+}
+
+module.exports = { getUserPost, getUserFriend, getUserProfileData };
